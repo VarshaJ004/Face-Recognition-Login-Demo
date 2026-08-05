d423390b6e75506a19898ca31e03d6a517e4bc64
VarshaJ004 <varshajohnson333@gmail.com>
2026-08-05 20:44:34 +0530

    Face Recoginition demo initated

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1>Face Recognition Login Demo</h1>
+
+      <button style={{ margin: "10px", padding: "10px 20px" }}>
+        Register Face
+      </button>
+
+      <button style={{ margin: "10px", padding: "10px 20px" }}>
+        Login with Face
+      </button>
+    </div>
+  );
+}
+
+export default App;
